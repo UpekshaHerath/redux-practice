cc255a316bd3e27cf86ae5b0d7c02424d054ff74
Upeksha Herath <upekshadilshan000@gmail.com>
2024-01-09 00:26:35 +0530

    documentation

--- src/App.js
+++ src/App.js
@@ -12,3 +12,16 @@ function App() {
 }
 
 export default App;
+
+/**
+ * 1. Create a store
+ * 2. Wrap the App using store using the provider
+ * 3. Create a slice
+ *      - Create the intial state of your data
+ *      - create the slice using createSlice method
+ *          - porvide a name for the slice
+ *          - give the initial state
+ *          - provide the reducers as many as you have like addTodo, removeTodo and so on...
+ *          - export the reducer out of the slice which created. Because this will be used in store.
+ *          - export the individual reducers and we have to use those. 
+ */
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,7 +5,12 @@ import React from 'react';
 */
 import {useSelector} from 'react-redux';
 
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+
 function Todos() {
+    const dispatch = useDispatch();
     const todos = useSelector(state => state.todos)
     console.log(todos);
 
@@ -14,9 +19,15 @@ function Todos() {
         <div>Todos</div>
         {
             todos.map((todo) => (
-                <div key={todo.id}>{todo.text}</div>
+                <div>
+                    <div key={todo.id}>
+                        {todo.text}
+                    </div>
+                    <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
+                </div>
             ))
         }
+
     </>
   )
 }
